3b9c3098724b482ea341a3f77ddd6d3f2cbd68a2
Ei Than Thar Aung <thanthar.24@ichat.sp.edu.sg>
2025-05-08 12:18:46 +0800

    BMI calculator

---
+++ bmi.py
@@ -0,0 +1,12 @@
+def calculate_bmi(height,weight):
+    print("Height = "+str(height))
+    print ("Weight = "+str(weight))
+    bmi=weight/(height*height)
+    print(f"BMI value= ",round(bmi,2))
+    if bmi< 18.5:
+        print("Under weight")
+    elif bmi>25.0:
+        print("Over weight")
+    else:
+        print("Normal weight")
+calculate_bmi(weight=37,height=1.73)
