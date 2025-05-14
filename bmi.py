73a7cd72108d35a7da9f7f5fd53f269b04373dda
Ei Than Thar Aung <thanthar.24@ichat.sp.edu.sg>
2025-05-14 15:10:46 +0800

    First python code

--- bmi.py
+++ bmi.py
@@ -5,8 +5,11 @@ def calculate_bmi(height,weight):
     print(f"BMI value= ",round(bmi,2))
     if bmi< 18.5:
         print("Under weight")
+        return -1
     elif bmi>25.0:
         print("Over weight")
+        return 1 
     else:
         print("Normal weight")
+        return 0
 calculate_bmi(weight=37,height=1.73)
